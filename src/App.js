8ea5d50da3a9076088d1511ec8ba22be88ecedd4
Jason Wodicka <jason@wodicka.net>
2021-11-05 15:12:16 -0700

    add a new page for slicer-testing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 import Puzzle from './Puzzle';
 
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <Puzzle
-        picture="img/jonathan-kemper-9tamF4J0vLk-unsplash.jpg"
-        pictureDimensions={{w: 6000, h: 4000}}
-        pieceDimensions={{w: 12, h: 8}}
-      />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/puzzle">
+            <Puzzle
+              picture="img/jonathan-kemper-9tamF4J0vLk-unsplash.jpg"
+              pictureDimensions={{w: 6000, h: 4000}}
+              pieceDimensions={{w: 12, h: 8}}
+            />
+          </Route>
+          <Route path="/slicer">
+            <h1>Slicer options will be here later.</h1> 
+          </Route>
+          <Route path="/">
+            <div>
+              <Link to="/puzzle">Puzzle</Link>
+              <Link to="/slicer">Slicer</Link>
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
