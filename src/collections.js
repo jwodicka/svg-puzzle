7dda92d9ec80a814dca933845d7176ed9a786fd9
Jason Wodicka <jason@wodicka.net>
2021-11-06 17:43:14 -0700

    Fix indentation

--- src/collections.js
+++ src/collections.js
@@ -1,30 +1,30 @@
 export const setIntersection = (setA, setB) => {
   let _intersection = new Set()
   for (let elem of setB) {
-      if (setA.has(elem)) {
-          _intersection.add(elem)
-      }
+    if (setA.has(elem)) {
+      _intersection.add(elem)
+    }
   }
   return _intersection
 }
 
 export const setDisjunction = (setA, setB) => {
   let _difference = new Set(setA)
-    for (let elem of setB) {
-        if (_difference.has(elem)) {
-            _difference.delete(elem)
-        } else {
-            _difference.add(elem)
-        }
+  for (let elem of setB) {
+    if (_difference.has(elem)) {
+      _difference.delete(elem)
+    } else {
+      _difference.add(elem)
     }
-    return _difference
+  }
+  return _difference
 }
 
 export const shuffle = (original) => {
   const array = [...original];
   for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 }
